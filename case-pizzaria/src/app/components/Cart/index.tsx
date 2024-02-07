3e18a235d5739dd52df4acefab96a94b3f60736b
Vinicius Duarte Passo <92999708+ViniciusDuarte17@users.noreply.github.com>
2024-02-06 21:20:45 -0300

    correção no fragment do cart

--- case-pizzaria/src/app/components/Cart/index.tsx
+++ case-pizzaria/src/app/components/Cart/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export const Cart = () => {
     return(
-        <React.Fragment className="border border-spacing-0">
+        <React.Fragment>
             <section>
                 <p>img-pizza</p>
             </section>
